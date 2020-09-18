37bec61600476a7d4f47c234bfc72b86a246d5dc
shakhovm <shakhov@ucu.edu.ua>
2020-09-18 17:13:30 +0300

    first test

--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,134 @@
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include <iostream>
 #include <boost/program_options.hpp>
+#include <vector>
+namespace po = boost::program_options;
+using namespace std;
 
-#include "operations/operations.hpp"
+int readbuffer(int fd, ssize_t size, char* buf, size_t& total_read) {
 
-int main(int argc, char **argv) {
-    int variable_a, variable_b;
+    ssize_t x = 0;
+    while (x < size) {
+        ssize_t add_size = read(fd, buf + x, size - x);
 
-    namespace po = boost::program_options;
+        if (add_size == -1) {
+            if (errno == EINTR) {
+                continue;
+            }
+            return -1;
+        }
+        else if (add_size == 0) {
+            break;
+        }
+        x += add_size;
+    }
+    total_read = x;
+    return 0;
+}
 
-    po::options_description visible("Supported options");
-    visible.add_options()
-            ("help,h", "Print this help message.");
+int writebuf(int fd, const char* buf, ssize_t s) {
+    ssize_t add_b = 0;
+    while (add_b < s) {
+        ssize_t now = write(fd, buf + add_b, s - add_b);
+        if (now == -1) {
+            if (errno != EINTR)
+            {
+                return -1;
+            }
 
-    po::options_description hidden("Hidden options");
+        }
+        else {
+            add_b += now;
+        }
+    }
+    return 0;
+}
+
+constexpr size_t buf_size = 512;
+typedef struct stat stat_t;
+
+int main(int argc, char** argv)
+{
+    char const hex_chars[16] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+    // Declare the supported options.
+    po::options_description hidden("Allowed options");
     hidden.add_options()
-            ("a", po::value<int>(&variable_a)->default_value(0), "Variable A.")
-            ("b", po::value<int>(&variable_b)->default_value(0), "Variable B.");
-
+        ("input-file", po::value< vector<string> >(), "input file")
+        ("show-all,A", "hex code")
+        ;
+    po::options_description cmdline_options;
+    cmdline_options.add(hidden);
     po::positional_options_description p;
-    p.add("a", 1);
-    p.add("b", 1);
-
-    po::options_description all("All options");
-    all.add(visible).add(hidden);
+    p.add("input-file", -1);
 
     po::variables_map vm;
-    po::store(po::command_line_parser(argc, argv).options(all).positional(p).run(), vm);
+    po::store(po::command_line_parser(argc, argv).
+              options(hidden).positional(p).run(), vm);
     po::notify(vm);
 
-    if (vm.count("help")) {
-        std::cout << "Usage:\n  add [a] [b]\n" << visible << std::endl;
-        return EXIT_SUCCESS;
+    if (vm.count("input-file"))
+    {
+//   //     cout << "Input files are: ";
+        std::vector<string> v(vm["input-file"].as< vector<string> >());
+//        for_each(v.begin(), v.end(), [](string& s){cout << s << " "; });
+        std::vector<int> fds;
+        int fd_file;
+        for (const auto& file_name: v) {
+            if ((fd_file = open(file_name.c_str(), O_RDONLY)) < 0) {
+
+                return 1;
+            }
+            stat_t st;
+            stat(file_name.c_str(), &st);
+            if (S_ISDIR(st.st_mode)) {
+                return 2;
+            }
+
+            fds.push_back(fd_file);
+        }
+        char buf[buf_size];
+        for (const auto& fd: fds) {
+            size_t x = -1;
+            while (x >= buf_size)
+            {
+                readbuffer(fd, sizeof buf, buf, x);
+                if (x == 0)
+                    break;
+                if (vm.count("show-all")) {
+
+                    std::stringstream s;
+                    std::string string;
+                    for (size_t i = 0; i < x; ++i) {
+
+                        if (isspace(buf[i]) || isprint(buf[i])) {
+                            string += buf[i];
+                        }
+
+                        else {
+                            string += "\\x";
+                            string += hex_chars[ ( buf[i] & 0xF0 ) >> 4 ];
+                            string += hex_chars[ ( buf[i] & 0xF ) >> 0 ];
+
+                        }
+                    }
+
+                    writebuf(STDOUT_FILENO, string.c_str(), string.size());
+                    continue;
+                }
+
+                writebuf(STDOUT_FILENO, buf, x);
+
+            }
+            close(fd);
+        }
+
+
+    }
+    if (vm.count("show-all"))
+    {
+
     }
 
-    int result = operations::add(variable_a, variable_b);
-    std::cout << result << std::endl;
-    return EXIT_SUCCESS;
 }
